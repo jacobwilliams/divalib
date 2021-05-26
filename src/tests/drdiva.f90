119d7b6b25c888f5b447348dc9de1206a858194a
Jacob Williams <jacobwilliams@users.noreply.github.com>
2021-05-25 21:37:47 -0500

    reformatted example
    docstring updates

--- src/tests/drdiva.f90
+++ src/tests/drdiva.f90
@@ -1,93 +1,114 @@
-      program DRDIVA
-!>> 2010-06-09 DRDIVA  Krogh Used parameters for all dimenssions.
-!>> 2001-05-25 DRDIVA  Krogh Minor change for making .f90 version.
-!>> 1996-06-14 DRDIVA  Krogh  Small change in output format
-!>> 1994-11-02 DRDIVA  Krogh  Changes to use M77CON
-!>> 1994-07-18 DRDIVA  Krogh   Last change.
-!--D replaces "?": DR?IVA, ?IVA, ?IVAF, ?IVAO
-! Sample driver for DIVA --  Set up to solve two second order equations.
+!*************************************************************************
+!>
+!  Sample driver for DIVA --  Set up to solve two second order equations.
 !
-      use diva_module
-      integer INEQ, IFDIM, IKDIM, ITDIM, IYDIM
-      parameter (INEQ=2,IFDIM=16*INEQ+1,IKDIM=6,ITDIM=4,IYDIM=4*INEQ)
-      integer          NEQ, KORD(IKDIM), IOPT(6)
-!--D Next line special: P=>D, X=>Q
-      double precision TSPECS(ITDIM), Y(IYDIM), T, H, DELT, TFINAL
-      integer NDIG
-      double precision F(IFDIM), TOL
-      equivalence (TSPECS(1), T), (TSPECS(2), H), (TSPECS(3), DELT),    &
-     &  (TSPECS(4), TFINAL)
-      external DIVAO, DIVAF
-!++SP Default NDIG = 4
-!++  Default NDIG = 10
-!++ Substitute for NDIG below
-      parameter (NDIG = 10)
-      parameter (TOL = 10.D0 **(-NDIG))
-!
-      data   NEQ,    T,    H,                   DELT, TFINAL /          &
-     &         2, 0.D0, 1.D0, 6.283185307179586477D0,   2.D1 /,         &
-     &       Y(1), Y(2), Y(3), Y(4) /                                   &
-     &       1.D0, 0.D0, 0.D0, 1.D0 /
-!
-! Set option for error control, local absolute error < TOL.
-      data   IOPT(1), IOPT(2), IOPT(3) /                                &
-     &            16,       6,   3 /,                                   &
-     &                KORD(6), F(3)/                                    &
-     &                      2,  TOL/
-! Group the system to be treated as a single unit, set tolerance value
-! Set option for second order equations
-      data   IOPT(4), IOPT(5) /                                         &
-     &            17,       2 /
-!  Flag end of options
-      data   IOPT(6) / 0/
-!
-! Do the integration
-!
-      KORD(1) = 0
-  100 continue
-      call DIVA(TSPECS, Y, F, KORD, NEQ, DIVAF, DIVAO,                  &
-     &  ITDIM, IYDIM, IFDIM, IKDIM, IOPT)
-      if (KORD(1) /= 1) go to 100
-      stop
-      END
+!### History
+!  * 2010-06-09 DRDIVA  Krogh Used parameters for all dimenssions.
+!  * 2001-05-25 DRDIVA  Krogh Minor change for making .f90 version.
+!  * 1996-06-14 DRDIVA  Krogh  Small change in output format
+!  * 1994-11-02 DRDIVA  Krogh  Changes to use M77CON
+!  * 1994-07-18 DRDIVA  Krogh   Last change.
 
-      subroutine DIVAF(T, Y, F, KORD)
-! Sample derivative subroutine for use with DIVA
-! This evaluates derivatives for a simple two body problem.
-!
-      integer          KORD
-!--D Next line special: P=>D, X=>Q
-      double precision T, Y(4), TP
-      double precision F(2)
-!
-! Evaluate the derivatives
-!
-      TP = Y(1)*Y(1) + Y(3)*Y(3)
-      TP = 1.D0 / (TP * SQRT(TP))
-      F(1) = -Y(1) * TP
-      F(2) = -Y(3) * TP
-      return
-      END
+    program DRDIVA
 
-      subroutine DIVAO(TSPECS, Y, F, KORD)
-! Sample output subroutine for use with DIVA.
-! This subroutine gives output for a simple two body problem.
-!
-      integer          KORD
-!--D Next line special: P=>D, X=>Q
-      double precision TSPECS(4), Y(4)
-      double precision F(2)
- 1000 format (12X,                                                      &
-     &   'RESULTS FOR A SIMPLE 2-BODY PROBLEM'//                        &
-     &   8X, 'T', 13X, 'U/V', 11X, 'UP/VP', 9X, 'UPP/VPP')
- 1001 format (1P,SP,4E15.6 / 15X, 3E15.6/' ')
-!
-! Do the output
-!
-      if (KORD == 1) then
-        write (*, 1000)
-      end if
-      write (*, 1001) TSPECS(1), Y(1), Y(2), F(1), Y(3), Y(4), F(2)
-!
-      return
-      END
+    use diva_module
+
+    implicit none
+
+    integer, parameter :: ineq  = 2
+    integer, parameter :: ifdim = 16*ineq+1
+    integer, parameter :: ikdim = 6
+    integer, parameter :: itdim = 4
+    integer, parameter :: iydim = 4*ineq
+    integer, parameter :: ndig = 10
+    double precision, parameter :: tol = 10.0d0 **(-ndig)
+
+    integer :: neq, kord(ikdim), iopt(6)
+    double precision :: tspecs(itdim), y(iydim), t, h, delt, tfinal
+    double precision :: f(ifdim)
+
+    neq         = 2
+    t           = 0.d0
+    h           = 1.d0
+    delt        = 6.283185307179586477d0
+    tfinal      = 2.d1
+    y(1:4)      = [1.d0, 0.d0, 0.d0, 1.d0 ]
+    tspecs(1:4) = [t, h, delt, tfinal]
+
+    ! set option for error control, local absolute error < tol.
+    iopt(1) = 16
+    iopt(2) = 6;    kord(6) = 2
+    iopt(3) = 3;    f(3) = tol
+
+    ! group the system to be treated as a single unit, set tolerance value
+    ! set option for second order equations
+    iopt(4) = 17
+    iopt(5) = 2
+
+    ! flag end of options
+    iopt(6) = 0
+
+    ! do the integration
+    kord(1) = 0
+    do
+        call diva(tspecs, y, f, kord, neq, divaf, divao, &
+                itdim, iydim, ifdim, ikdim, iopt)
+        if (kord(1) == 1) exit
+    end do
+
+    contains
+!*************************************************************************
+
+    !*************************************************************************
+        subroutine divaf(t, y, f, kord)
+
+        !! Sample derivative subroutine for use with DIVA
+        !! This evaluates derivatives for a simple two body problem.
+
+        implicit none
+
+        integer :: kord
+        double precision :: t
+        double precision :: y(4)
+        double precision :: f(2)
+
+        double precision :: tp
+
+        ! Evaluate the derivatives
+        tp = y(1)*y(1) + y(3)*y(3)
+        tp = 1.d0 / (tp * sqrt(tp))
+        f(1) = -y(1) * tp
+        f(2) = -y(3) * tp
+
+        end subroutine divaf
+    !*************************************************************************
+
+    !*************************************************************************
+        subroutine divao(tspecs, y, f, kord)
+
+        !! Sample output subroutine for use with DIVA.
+        !! This subroutine gives output for a simple two body problem.
+
+        implicit none
+
+        integer :: kord
+        double precision :: tspecs(4)
+        double precision :: y(4)
+        double precision :: f(2)
+
+        ! Do the output
+        if (kord == 1) then
+           write (*, '(12X,A//,8X,A,13X,A,11X,A,9X,A)') &
+                     'RESULTS FOR A SIMPLE 2-BODY PROBLEM',&
+                     'T','U/V','UP/VP','UPP/VPP'
+        end if
+        write (*, '(1P,SP,4E15.6/ 15X,3E15.6/ A)') &
+                  tspecs(1), y(1), y(2), f(1), y(3), y(4), f(2), ' '
+
+        end subroutine divao
+    !*************************************************************************
+
+    end program drdiva
+!*************************************************************************
+
+
